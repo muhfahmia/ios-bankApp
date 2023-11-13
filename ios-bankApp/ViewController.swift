621e2f85b3688e31149aad92c04b1c0686ed83cd
Fahmi Ardiyanto <mfahmiardiyannto@gmail.com>
2023-11-13 15:20:34 +0700

    Setup Project

---
+++ ios-bankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-bankApp
+//
+//  Created by Muhammad Fahmi on 13/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
